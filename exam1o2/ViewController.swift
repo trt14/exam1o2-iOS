cb096069c3467166b1a7df123569279e94c61f74
trt14 <tarooti14dev@gmail.com>
2021-12-09 09:54:13 +0300

    Initial Commit

---
+++ exam1o2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exam1o2
+//
+//  Created by Tarooti on 05/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
